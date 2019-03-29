25334a2a2b4a87d293c694ca35f30a9565568461
sungmin414 <sungmin3231@gmail.com>
2019-03-29 19:57:10 +0900

    results뷰 구현

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -30,10 +30,32 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    context = {
+        'question': get_object_or_404(Question, pk=question_id)
+    }
+    return render(request, 'polls/result.html', context)
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    # question_id가 pk인 Question객체를 DB로부터 가져온 데이터로 생성
+    # 만약 해당하는 Question이 없다면 Http404예외가 발생함
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # 현재 투표중인 Question에 속한 Choice목록에서
+        # pk가 POST요청에 전달된 'choice'값에 해당하는 Choice객체를 selected_choice변수에 할당
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 위의 try문에서 발생할 수 있는 예외는 2가지
+        # 1. KeyError: request.POST에 'choice'키가 없을 때 발생
+        # 2. Choice.DoesNotExist: question.choice_set.get(pk=무언가)에서 발생
+        # (pk에 해당하는 객체가 DB에 없을 경우)
+        context = {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        }
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect('polls:results', question_id)
 
