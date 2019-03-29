e1a9b06e6c20df95dc11f43994d1da0de8d40b56
sungmin414 <sungmin3231@gmail.com>
2019-03-29 16:44:58 +0900

    index, detail, results, vote view를 url과 연결, index뷰는 가장 최근에 발행된 Question을 5개까지 보여줌

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,8 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world, You're at the polls index")
+    # Question 클래스에 대한 QuerySet 을 가져옴
+    # 게시일자 속성에 대한 내림차순 순서로 최대 5개까지
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
 
+    # 가져온 Question QuerySet 을 사용, 각 Question의 question_text속성값들을 list comprehension 을 사용해 리스트로 생성
+    # 생성한 리스트를 ', '  문자열의 join메서드의 인수로 전달 output에 쉼표단위로 연결된 문자열을 할당
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
 
