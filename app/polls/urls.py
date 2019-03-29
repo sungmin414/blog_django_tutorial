99ea81a07280e10f552ef8fbe64874f6110b76e6
sungmin414 <sungmin3231@gmail.com>
2019-03-29 20:38:41 +0900

    IndexView로 /polls/cbv/구현

--- app/polls/urls.py
+++ app/polls/urls.py
@@ -1,14 +1,23 @@
-from django.urls import path
+from django.urls import path, include
+
 from . import views
-from .views import fbv as views
+from .views import cbv,fbv as views
 
 
 app_name = 'polls'
+
+urlpatterns_cbv = [
+    path('', cbv.IndexView.as_view(), name='index')
+]
 
 urlpatterns = [
     path('', views.index, name='index'),
     # r'^(?P<question_id>\d+)/$'
     path('<int:question_id>/', views.detail, name="detail"),
     path('<int:question_id>/results/', views.results, name="results"),
-    path('<int:question_id>/vote/', views.vote, name="vote")
+    path('<int:question_id>/vote/', views.vote, name="vote"),
+
+    # /polls/cbv/로 시작하는 URL요청은
+    # 위의 urlpatterns_cbv리스트 내의 내용에서 처리
+    path('cbv/', include(urlpatterns_cbv)),
 ]
--- app/polls/views/fbv.py
+++ app/polls/views/fbv.py
@@ -1,4 +1,5 @@
 # fbv(function-base view)
+
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
